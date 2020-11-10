b8311a52c0b5b90d8db89271406434c32b316a3c
amaury-diallo <amadoury313@gmail.com>
2020-11-10 10:20:24 +0000

    Protected routes with token persistence

--- components/auth/protect-route.js
+++ components/auth/protect-route.js
@@ -1,14 +1,23 @@
 import { useEffect, useState } from "react";
 import protectedRoutes from "../../configs/auth";
 import { withRouter } from "next/router";
-import { useStoreState } from "easy-peasy";
+import { useStoreActions, useStoreState } from "easy-peasy";
 import Loader from "../loader/Loader";
+import Cookies from "js-cookie";
+import { appRoutes } from "../../configs/routes";
 
 const ProtectRoute = ({ children, router }) => {
   const [checking, setChecking] = useState(true);
   const loginUrl = "/login";
   const user = useStoreState((state) => state.auth.user);
   const isUserLoggedIn = useStoreState((state) => state.auth.isUserLoggedIn);
+  const loadUserFromCookies = useStoreActions(
+    (state) => state.auth.loadUserFromCookies
+  );
+
+  const token = Cookies.get("token");
+
+  if (token) loadUserFromCookies(token);
 
   useEffect(() => {
     const isProtectedRoute = !!protectedRoutes.find((r) => r === router.route);
@@ -29,6 +38,11 @@ const ProtectRoute = ({ children, router }) => {
         });
 
       console.log("Protected route");
+    } else if (
+      isUserLoggedIn &&
+      (router.route === appRoutes.login || router.route === appRoutes.register)
+    ) {
+      setChecking(true);
     } else {
       setChecking(false);
     }
--- store/models/user.js
+++ store/models/user.js
@@ -1,11 +1,23 @@
 import { action, thunk, computed } from "easy-peasy";
 import API from "../../services/axios";
+import Cookies from "js-cookie";
+import { appRoutes } from "../../configs/routes";
+import { Router } from "next/router";
 
 const userModel = {
   currentUser: {},
+  loadUserFromCookies: thunk(async (actions, userToken) => {
+    console.log("Got a token in the cookies, let's see if it is valid");
+    API.defaults.headers.Authorization = `Bearer ${userToken}`;
+    const { data: user } = await API.get("/users/current");
+    console.log(user);
+    if (user) actions.setUser(user);
+  }),
   loginUser: thunk(async (actions, user) => {
     const { data } = await API.post("/users/authenticate", user);
     // console.log(data);
+    Cookies.set("token", data.token, { expires: 60 });
+    API.defaults.headers.Authorization = `Bearer ${data.token}`;
 
     actions.setUser(data);
   }),
@@ -15,8 +27,10 @@ const userModel = {
   setUser: action((state, user) => {
     state.currentUser = user;
   }),
-  logoutUser: action((state, user) => {
-    state.currentUser = {};
+  logoutUser: thunk((actions) => {
+    Cookies.remove("token");
+    delete API.defaults.headers.Authorization;
+    actions.setUser({});
   }),
 };
 
